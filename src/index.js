13c8b8a27fcd68abf51712800c9c23d6cd3b1a36
liuzhu <1834448946@qq.com>
2024-11-19 02:16:24 +0800

    d

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ var mysql = require('mysql');
 
 // 配置数据库连接参数
 const connection = mysql.createConnection({
-  host: '172.23.215.76', // 替换为你的阿里云服务器的IP地址
+  host: 'localhost', // 替换为你的阿里云服务器的IP地址
   user: 'root', // 替换为你的数据库用户名
   password: 'Goupazhai<123', // 替换为你的数据库密码
-  database: 'performance_schema', // 替换为你的数据库名
+  database: 'pbootcms', // 替换为你的数据库名
   port: 3306 // MySQL的默认端口是3306
 });
  
