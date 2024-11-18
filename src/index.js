e82c14c9c1787206f023040549a4ff6ff7c6e28a
liuzhu <1834448946@qq.com>
2024-11-19 01:40:17 +0800

    feat: 试下

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 //express_demo.js 文件
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
+// 配置数据库连接参数
+const connection = mysql.createConnection({
+  host: '172.23.215.76', // 替换为你的阿里云服务器的IP地址
+  user: 'root', // 替换为你的数据库用户名
+  password: 'Goupazhai<123', // 替换为你的数据库密码
+  database: 'mysql', // 替换为你的数据库名
+  port: 3306 // MySQL的默认端口是3306
+});
+ 
+// 开启数据库连接
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Successfully connected to the database.');
+ 
+  // 关闭数据库连接
+  connection.end();
+});
  
 app.get('/', function (req, res) {
    res.send('Hello World1');
