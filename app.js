0c673664f4eaa33a3768dc172b5c6e080cff4e6a
liuzhu <1834448946@qq.com>
2024-11-20 00:47:55 +0800

    chu bu de ssr

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const reactSsr  = require('./dist/src/server/middlewares/react-ssr').default;
+
+const app = express();
+
+// 静态文件处理
+app.use(express.static(path.join(__dirname, 'public')));
+// 将打包好的静态文件也放到静态文件服务下
+app.use(express.static(
+    path.join(__dirname, './dist/static')
+));
+
+// 首页
+app.use("/" ,reactSsr);
+
+app.listen(9001,  function () {
+    console.log("服务启动完毕");
+});
